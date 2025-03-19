3e08ac415b3f42590894e21a79e8c2ff6652afb2
MarksSondors <sondorsmarks@gmail.com>
2025-03-19 14:50:49 +0200

    Add custom user model, update login template, and configure static files

--- entertainment/custom_auth/views.py
+++ entertainment/custom_auth/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def 
+def login_page(request):
+    if request.method == 'POST':
+        # Handle the form submission
+        pass
+    return render(request, 'login_page.html')
--- entertainment/entertainment/settings.py
+++ entertainment/entertainment/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Third-party apps
+    'django_bootstrap5',
+
+    # Custom apps
     'custom_auth',  # user info and tables which are not unique to other apps
     'books',
     'movies',
@@ -122,6 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
