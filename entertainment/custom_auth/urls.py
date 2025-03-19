4dcdbde5fb56946f14c72d9f27c3b53bff124df1
MarksSondors <sondorsmarks@gmail.com>
2025-03-19 15:03:03 +0200

    Add home page template, update login handling, and configure URLs

--- entertainment/custom_auth/urls.py
+++ entertainment/custom_auth/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # pages
     path('', views.login_page, name='login_page'),
+    path('home/', views.home_page, name='home'),
+
+    # requests
+    path('login/', views.login_request, name='login_request'),
+
 ]
--- entertainment/custom_auth/views.py
+++ entertainment/custom_auth/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 # Create your views here.
 
 def login_page(request):
-    if request.method == 'POST':
-        # Handle the form submission
-        pass
     return render(request, 'login_page.html')
+
+def login_request(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return redirect('login_page')
+    return render(request, 'login_page.html')
+
+def home_page(request):
+    if request.user.is_authenticated:
+        return render(request, 'home_page.html')
+    else:
+        return redirect('login_page')
