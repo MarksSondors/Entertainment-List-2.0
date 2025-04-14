653715c8a774f975311ddadaaa657069b2ca732f
MarksSondors <sondorsmarks@gmail.com>
2025-04-14 21:13:38 +0300

    Refactor PersonAdmin class to display connected movies with clickable links in the admin interface

--- entertainment/custom_auth/admin.py
+++ entertainment/custom_auth/admin.py
@@ -1,13 +1,66 @@
 from django.contrib import admin
+from django.contrib.contenttypes.models import ContentType
+from django.urls import reverse
+from django.utils.html import format_html
 
 # Register your models here.
-from .models import CustomUser, Genre, Keyword, Country, Watchlist, Review
+from .models import CustomUser, Genre, Keyword, Country, Watchlist, Review, Person, MediaPerson
+from movies.models import Movie
 
 admin.site.register(CustomUser)
 admin.site.register(Genre)
 admin.site.register(Keyword)
 admin.site.register(Country)
 admin.site.register(Watchlist)
+
+
+class PersonAdmin(admin.ModelAdmin):
+    search_fields = ['name', 'imdb_id']
+    list_display = ('name', 'is_director', 'is_writer', 'is_original_music_composer')
+    list_filter = ('is_director', 'is_writer', 'is_screenwriter', 'is_story', 'is_original_music_composer')
+    readonly_fields = ('display_connected_movies',)
+    
+    def display_connected_movies(self, obj):
+        movie_content_type = ContentType.objects.get_for_model(Movie)
+        
+        # Get all MediaPerson entries for this person related to movies
+        media_persons = MediaPerson.objects.filter(
+            person=obj,
+            content_type=movie_content_type
+        )
+        
+        if not media_persons.exists():
+            return "-"
+        
+        # Get all movies in one query
+        movie_ids = media_persons.values_list('object_id', flat=True).distinct()
+        movies = {movie.id: movie for movie in Movie.objects.filter(id__in=movie_ids)}
+        
+        # Organize roles by movie
+        movie_roles = {}
+        for mp in media_persons:
+            if mp.object_id not in movie_roles:
+                movie_roles[mp.object_id] = []
+            
+            role_info = mp.role
+            if mp.character_name:
+                role_info += f" as {mp.character_name}"
+            movie_roles[mp.object_id].append(role_info)
+        
+        # Build HTML list with clickable links to movie admin pages
+        html = "<ul>"
+        for movie_id in sorted(movie_roles.keys(), key=lambda x: movies[x].title if x in movies else ""):
+            if movie_id in movies:
+                movie = movies[movie_id]
+                url = reverse('admin:movies_movie_change', args=[movie_id])
+                html += f'<li><a href="{url}">{movie.title}</a> ({", ".join(movie_roles[movie_id])})</li>'
+        html += "</ul>"
+        return format_html(html)
+    
+    display_connected_movies.short_description = "Connected Movies"
+
+
+admin.site.register(Person, PersonAdmin)
 
 @admin.register(Review)
 class ReviewAdmin(admin.ModelAdmin):
--- entertainment/movies/admin.py
+++ entertainment/movies/admin.py
@@ -8,54 +8,6 @@ from .models import *
 
 admin.site.register(Collection)
 
-class PersonAdmin(admin.ModelAdmin):
-    search_fields = ['name', 'imdb_id']
-    list_display = ('name', 'is_director', 'is_writer', 'is_original_music_composer')
-    list_filter = ('is_director', 'is_writer', 'is_screenwriter', 'is_story', 'is_original_music_composer')
-    readonly_fields = ('display_connected_movies',)
-    
-    def display_connected_movies(self, obj):
-        movie_content_type = ContentType.objects.get_for_model(Movie)
-        
-        # Get all MediaPerson entries for this person related to movies
-        media_persons = MediaPerson.objects.filter(
-            person=obj,
-            content_type=movie_content_type
-        )
-        
-        if not media_persons.exists():
-            return "-"
-        
-        # Get all movies in one query
-        movie_ids = media_persons.values_list('object_id', flat=True).distinct()
-        movies = {movie.id: movie for movie in Movie.objects.filter(id__in=movie_ids)}
-        
-        # Organize roles by movie
-        movie_roles = {}
-        for mp in media_persons:
-            if mp.object_id not in movie_roles:
-                movie_roles[mp.object_id] = []
-            
-            role_info = mp.role
-            if mp.character_name:
-                role_info += f" as {mp.character_name}"
-            movie_roles[mp.object_id].append(role_info)
-        
-        # Build HTML list with clickable links to movie admin pages
-        html = "<ul>"
-        for movie_id in sorted(movie_roles.keys(), key=lambda x: movies[x].title if x in movies else ""):
-            if movie_id in movies:
-                movie = movies[movie_id]
-                url = reverse('admin:custom_auth_movie_change', args=[movie_id])
-                html += f'<li><a href="{url}">{movie.title}</a> ({", ".join(movie_roles[movie_id])})</li>'
-        html += "</ul>"
-        return format_html(html)
-    
-    display_connected_movies.short_description = "Connected Movies"
-
-
-admin.site.register(Person, PersonAdmin)
-
 class MediaPersonInline(GenericTabularInline):
     model = MediaPerson
     extra = 1
